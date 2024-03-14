fbdb1a096ca684dbddb7ed20360f9f9f07836032
sohvi13 <sohvi.haukkala@gmail.com>
2024-03-14 16:02:11 +0200

    melkein kaikki
    siirretään  melkein kaikki html tehtävät

---
+++ Js/helloworld.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    console.log("Tämä kirjoitetaan selaimen konsoliin");
+}
+function sayHelloTo(name) {
+    alert("Hei " + name + "!");
+    console.log("sayHello-funktio suoritettu");
+    }
