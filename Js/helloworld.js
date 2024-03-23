e1f7322ce97a8e5eca46d1b1e31ee9277eb455ed
sohvi13 <sohvi.haukkala@gmail.com>
2024-03-23 14:32:54 +0200

    Kartta toimimaan
    Kartta korjattiin niin, että se toimii

--- Js/helloworld.js
+++ Js/helloworld.js
@@ -1,7 +1,23 @@
+//Tässä annetaan napille käyttö funktio
+
+//Kun Hae-painiketta klikataan, päivitetään kartta. 
+//Javascript-funktiossa haetaan ensin tekstikenttien 
+//(Katuosoite ja Kaupunki) arvot ja sen jälkeen päivitetään 
+//iframe-elementin src-attribuutin arvo.
+function searchForm() {
+    var address = document.getElementById('textField1').value;
+    var city = document.getElementById('textField2').value;
+    var mapIframe = document.getElementById('map-iframe');
+    
+    // Rakennetaan uusi kartan url-osoite käyttäjän antamien tietojen perusteella
+    var mapUrl = "https://www.google.com/maps?q=" + encodeURI(address + " " + city) + "&output=embed";
+    
+    // Päivitetään iframe-elementin src-attribuutti uudella url-osoitteella
+    mapIframe.src = mapUrl;
+  }
+
+
+// Kutsutaan updateMap-funktiota sivun latautuessa, jotta kartta päivittyy oletusosoitteen perusteella
 window.onload = function() {
-    console.log("Tämä kirjoitetaan selaimen konsoliin");
-}
-function sayHelloTo(name) {
-    alert("Hei " + name + "!");
-    console.log("sayHello-funktio suoritettu");
-    }
+    updateMap();
+  };
